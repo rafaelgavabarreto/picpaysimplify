be855597fd59b41e313be850265b8528fa57fa98
Rafael Barreto <rafaelgavabarreto@gmail.com>
2023-09-01 08:53:32 -0400

    First commit

---
+++ src/main/java/com/picpaysimplify/PicpaysimplifyApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysimplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimplifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpaysimplify/PicpaysimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaysimplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
